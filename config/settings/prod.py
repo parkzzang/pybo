b087588617b9283ef7342b3cd545dc2e931d6592
parkzzang <parkzzang052@naver.com>
2025-03-17 21:46:17 +0900

    DNS 재설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.69.171', 'pybo.kr']
+ALLOWED_HOSTS = ['3.39.69.171', 'pybobo.duckdns.org']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
